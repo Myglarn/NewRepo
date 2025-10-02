34f9b0085eb3cd857fe9af4a5e6623e2d1c1fab3
Myglarn <christopher.petti@gmail.com>
2025-10-02 13:56:20 +0200

    Hejsan

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jek
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
